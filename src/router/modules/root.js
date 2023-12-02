9b22a206fb71ded45c7fe8bd84d4a31c1c931673
“chenjinwei” <507725948@qq.com>
2023-12-02 14:33:59 +0800

    刷新配置

--- src/router/modules/root.js
+++ src/router/modules/root.js
@@ -2,7 +2,7 @@
  * @Author: “chenjinwei” “507725948@qq.com”
  * @Date: 2023-12-02 08:39:37
  * @LastEditors: “chenjinwei” “507725948@qq.com”
- * @LastEditTime: 2023-12-02 08:59:59
+ * @LastEditTime: 2023-12-02 14:31:46
  * @FilePath: \vue2-template\src\router\modules\root.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ export default [
     },
     {
         path: '/home',
-        component: () => import('@/views/home'),
+        component: () => import(/* webpackChunkName: 'home' */'@/views/home.vue'),
         meta: {
             title: '主页'
         }
