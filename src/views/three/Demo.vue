c2f66e673814ddce2c100c80e0fc2a991c6a0096
“chenjinwei” <507725948@qq.com>
2023-12-18 17:02:09 +0800

    清除所有模型

--- src/views/three/Demo.vue
+++ src/views/three/Demo.vue
@@ -2,7 +2,7 @@
  * @Author: “chenjinwei” “507725948@qq.com”
  * @Date: 2023-12-18 16:24:35
  * @LastEditors: “chenjinwei” “507725948@qq.com”
- * @LastEditTime: 2023-12-18 16:39:48
+ * @LastEditTime: 2023-12-18 17:01:07
  * @FilePath: \maticalboy.github.io\src\views\three\Demo.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -20,7 +20,10 @@ export default {
     },
     mounted() {
         this.demo = new ThreeJSInitializer("threeDemo");
-        this.demo.init()
+        this.demo.init();
+        while (this.demo.scene.children.length > 0) {
+            this.demo.scene.remove(this.demo.scene.children[0]);
+        }
     },
 };
 </script>
