1bff4ce57f22f2d2c92b56234e3b14233bb90393
“chenjinwei” <507725948@qq.com>
2023-12-19 17:19:32 +0800

    更新相机的距离

--- src/utils/three/VR.js
+++ src/utils/three/VR.js
@@ -2,7 +2,7 @@
  * @Author: “chenjinwei” “507725948@qq.com”
  * @Date: 2023-12-18 16:21:12
  * @LastEditors: “chenjinwei” “507725948@qq.com”
- * @LastEditTime: 2023-12-19 16:27:53
+ * @LastEditTime: 2023-12-19 16:38:27
  * @FilePath: \maticalboy.github.io\src\utils\threeDemo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,7 +30,7 @@ export default class ThreeJSInitializer {
         const width = container.clientWidth;
         const height = container.clientHeight;
         this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-        this.camera.position.set(0, 0, 10);
+        this.camera.position.set(0, 0, 5);
 
         // 创建渲染器
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
